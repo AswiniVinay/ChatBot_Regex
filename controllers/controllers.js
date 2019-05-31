025d2bb2f195d9956e548eabc8c9c4256a9bd238
Aswini Vinay Soreng <aswin.soreng@gmail.com>
2019-06-01 00:58:33 +0530

    Add files via upload

---
+++ controllers/controllers.js
@@ -0,0 +1,13 @@
+module.exports=function(app){
+//req handlers
+
+app.get('/',function(req,res){
+
+});
+app.post('/',function(req,res){
+    
+});
+app.delete('/',function(req,res){
+    
+});
+};
---
+++ public/main.js
@@ -0,0 +1,47 @@
+﻿$(document).ready(function(){
+    $("button").click(function(){
+      let userip= $("#user-input").val();
+      console.log(userip);
+    var bot = new RiveScript();
+    bot.loadFile("brain.rive").then(loading_done).catch(loading_error);
+    function loading_done() {
+      console.log("Bot has finished loading!");
+    
+      // Now the replies must be sorted!
+      bot.sortReplies();
+      let username = "local-user";
+      bot.reply(username,userip).then(function(reply) {
+        console.log("The bot says: " + reply);
+        $("#output").html(reply);
+      });
+    }
+    
+    // It's good to catch errors too!
+    function loading_error(error, filename, lineno) {
+      console.log("Error when loading files: " + error);
+    }
+});
+$(document).on('keypress',function(e) {
+    if(e.which == 13) {
+        //see for enter key
+        let userip= $("#user-input").val();
+        console.log(userip);
+      var bot = new RiveScript();
+      bot.loadFile("brain.rive").then(loading_done).catch(loading_error);
+      function loading_done() {
+        console.log("Bot has finished loading!");
+      
+        // Now the replies must be sorted!
+        bot.sortReplies();
+        let username = "local-user";
+        bot.reply(username,userip).then(function(reply) {
+          console.log("The bot says: " + reply);
+          $("#output").html(reply);
+        });
+      }
+      function loading_error(error, filename, lineno) {
+        console.log("Error when loading files: " + error);
+      }   
+    }
+});
+});
